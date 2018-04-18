9c65e1ea469ddf21f821c479bedb7cbb59ed5bc1
vineetzunjarwad <vineetzunjarwad@gmail.com>
2018-04-18 13:54:04 -0700

    Breads API commit

---
+++ Bread-API/src/BreadAPI/server.go
@@ -0,0 +1,64 @@
+/*
+	Starbucks API in Go 
+
+*/
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"encoding/json"
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"github.com/unrolled/render"
+	"gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson"
+)
+
+
+// MongoDB Config
+var mongodb_server = "localhost:27017"
+var mongodb_database = "cmpe_breads"
+var mongodb_collection = "starbucks_breads"
+
+
+
+// NewServer configures and returns a Server.
+func NewServer() *negroni.Negroni {
+	formatter := render.New(render.Options{
+		IndentJSON: true,
+	})
+	n := negroni.Classic()
+	mx := mux.NewRouter()
+	initRoutes(mx, formatter)
+	n.UseHandler(mx)
+	return n
+}
+
+
+
+// API Routes
+func initRoutes(mx *mux.Router, formatter *render.Render) {
+	mx.HandleFunc("/breads", homepageHandlerSandwhich(formatter)).Methods("GET")
+}
+
+
+
+// Helper Functions
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
+
+
+
+// Ping Application
+func homepageHandlerSandwhich(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		formatter.JSON(w, http.StatusOK, struct{ Test string }{"Welcome to Starbucks Breads!"})
+	}
+}
---
+++ Bread-API/src/BreadAPI/types.go
@@ -0,0 +1,14 @@
+/*
+	Starbucks API in Go 
+	Uses MongoDB 
+*/
+	
+package main
+
+type starbucks struct {
+	cost     		int 	
+	likes			int     
+	item		 	string	   		
+	status 			int		
+	typeService 	string	
+}
