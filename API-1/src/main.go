224d5f34af0d723fbeaac07a6c42cba4b6555a59
AnshulPatni <anshulpatni19@gmail.com>
2018-04-17 17:15:32 -0700

    Added the server and port details

---
+++ API-1/src/main.go
@@ -0,0 +1,21 @@
+/*
+	Starbucks API in Go
+	Uses MongoDB
+*/
+	
+package main
+
+import (
+	"os"
+)
+
+func main() {
+
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "3000"
+	}
+
+	server := NewServer()
+	server.Run(":" + port)
+}
