d504a62665a92c1cb2992df3dcb4d5d1abe6d738
Zeeshan Ali <zeeshanalidba@gmail.com>
2018-04-18 21:40:56 -0700

    variables set, server added

---
+++ Smoothies-API/src/Smoothies/server.go
@@ -0,0 +1,43 @@
+/*
+	CmpE281 - Starbucks - Backend APIs
+
+*/
+
+package main
+
+import (
+	"strconv"
+	"fmt"
+	"log"
+	"net/http"
+	"encoding/json"
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"github.com/unrolled/render"
+	"gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson"
+)
+
+
+// MongoDB Config
+var mongodb_server = "localhost:27017"
+var mongodb_database = "cmpe281"
+var mongodb_collection = "starbucksSmoothies"
+
+
+
+
+// NewServer configures and returns a Server.
+func NewServer() *negroni.Negroni {
+	formatter := render.New(render.Options{
+		IndentJSON: true,
+	})
+	n := negroni.Classic()
+	mx := mux.NewRouter()
+	initRoutes(mx, formatter)
+	n.UseHandler(mx)
+	return n
+}
+
+
+
