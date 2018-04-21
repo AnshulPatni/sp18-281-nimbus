e225dd760f54101ad1b2cc5658336c7d3c763f65
Zeeshan Ali <zeeshanalidba@gmail.com>
2018-04-20 21:16:06 -0700

    added helper function, api routes, api's for - add to menu, search menu

--- Smoothies-API/src/Smoothies/server.go
+++ Smoothies-API/src/Smoothies/server.go
@@ -41,3 +41,77 @@ func NewServer() *negroni.Negroni {
 
 
 
+// API Routes
+func initRoutes(mx *mux.Router, formatter *render.Render) {
+
+	mx.HandleFunc("/inventorySmoothies", inventoryHandlerSmoothies(formatter)).Methods("GET")
+	mx.HandleFunc("/searchInventorySmoothies", searchInventoryHandlerSmoothies(formatter)).Methods("GET")
+
+}
+
+
+
+// Helper Functions
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
+
+
+
+
+//API returns  inventory for Status 0 items  to populate menu - 
+func inventoryHandlerSmoothies(formatter *render.Render) http.HandlerFunc {
+
+
+	return func(w http.ResponseWriter, req *http.Request) {
+
+		//establishing session with DB
+		session, err := mgo.Dial(mongodb_server)
+        if err != nil {
+                panic(err)
+        }
+        defer session.Close()
+        session.SetMode(mgo.Monotonic, true)
+        c := session.DB(mongodb_database).C(mongodb_collection)
+
+		var result []*bson.M
+
+        err = c.Find(nil).All(&result)
+
+        fmt.Println("Inventory details:", result )
+        formatter.JSON(w, http.StatusOK, result)
+
+
+	}
+}
+
+
+
+
+
+// API seacrhes inventory for a specific item and returns it
+func searchInventoryHandlerSmoothies(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		session, err := mgo.Dial(mongodb_server)
+        if err != nil {
+                panic(err)
+        }
+        defer session.Close()
+        session.SetMode(mgo.Monotonic, true)
+        c := session.DB(mongodb_database).C(mongodb_collection)
+        var result bson.M
+
+        err = c.Find(bson.M{"item" : "mocha"}).One(&result)
+
+        if err != nil {
+                log.Fatal(err)
+        }
+        fmt.Println("Inventory details:", result )
+		formatter.JSON(w, http.StatusOK, result)
+	}
+}
+
+
