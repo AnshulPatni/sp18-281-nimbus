7b344f3e5ab8a4687ede77b1b2f8fd6e7a63d52b
Zeeshan Ali <zeeshanalidba@gmail.com>
2018-04-29 11:16:11 -0700

    Changes to incorporate CORS

--- Smoothies-API/src/Smoothies/server.go
+++ Smoothies-API/src/Smoothies/server.go
@@ -6,38 +6,23 @@
 package main
 
 import (
-	"strconv"
-	"fmt"
-	"log"
-	"net/http"
-	"encoding/json"
-	"github.com/codegangsta/negroni"
-	"github.com/gorilla/mux"
-	"github.com/unrolled/render"
-	"gopkg.in/mgo.v2"
-    "gopkg.in/mgo.v2/bson"
+        "strconv"
+        "fmt"
+        "log"
+        "net/http"
+        "github.com/unrolled/render"
+        "gopkg.in/mgo.v2"
+        "gopkg.in/mgo.v2/bson"
 )
 
 
-// MongoDB Config
-var mongodb_server = "localhost:27017"
+//var mongodb_server = "localhost:27017"
+var mongodb_server = "mongodb://10.0.2.131:27017,10.0.2.120:27017,10.0.2.220:27017,10.0.0.101:27017,10.0.0.185:27017/?replicaSet=replica"
 var mongodb_database = "cmpe281"
 var mongodb_collection = "starbucksSmoothies"
 
 
 
-
-// NewServer configures and returns a Server.
-func NewServer() *negroni.Negroni {
-	formatter := render.New(render.Options{
-		IndentJSON: true,
-	})
-	n := negroni.Classic()
-	mx := mux.NewRouter()
-	initRoutes(mx, formatter)
-	n.UseHandler(mx)
-	return n
-}
 
 
 
