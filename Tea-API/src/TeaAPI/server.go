d6c110ec00996ec90924b64c425ae7db6721a143
Vasu <vasu.bansal@sjsu.edu>
2018-05-02 14:51:43 -0700

    Backend

--- Tea-API/src/TeaAPI/server.go
+++ Tea-API/src/TeaAPI/server.go
@@ -147,3 +147,120 @@ fmt.Println( "In add to cart!" )
 }
 
 
+
+func starbucksProcessOrdersHandlerTeas(rw http.ResponseWriter, req *http.Request) {
+
+
+	if origin := req.Header.Get("Origin"); origin != "" {
+	rw.Header().Set("Access-Control-Allow-Origin", origin)
+	rw.Header().Set("Content-Type", "application/json")
+	rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	rw.Header().Set("Access-Control-Allow-Headers",
+		"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
+
+	//establishing session with DB
+  session, err := mgo.Dial(mongodb_server)
+	  if err != nil {
+			  panic(err)
+	  }
+	  defer session.Close()
+	  session.SetMode(mgo.Monotonic, true)
+	  c := session.DB(mongodb_database).C(mongodb_collection)
+	
+
+	//this is just for testing purposes. It is a simulation of the request body from the client.
+	  var sim []bson.M
+	  err = c.Find(bson.M{ "status" : 1}).All(&sim)
+	  if err != nil {
+			  log.Fatal(err)
+	  } 
+
+	  //travering throuhg request body and updating each bson based on condition.
+	  for _, ele := range sim {
+		fmt.Println("Item status changing from 1 to 0 :", ele )
+		query := bson.M{"status" : 1}
+		change := bson.M{"$set": bson.M{ "status" : 0}}
+
+		err = c.Update(query, change)
+		if err != nil {
+			  log.Fatal(err)
+		} 
+
+	  } 
+
+				formatter := render.New(render.Options{
+						IndentJSON: true,
+					})
+		 
+
+		 
+	  fmt.Println("Items processed:", sim )
+	  formatter.JSON(rw, http.StatusOK, sim) 
+
+}
+
+
+
+
+func likeHandlerTeas(rw http.ResponseWriter, req *http.Request) {
+
+
+	if origin := req.Header.Get("Origin"); origin != "" {
+	rw.Header().Set("Access-Control-Allow-Origin", origin)
+	rw.Header().Set("Content-Type", "application/json")
+	rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	rw.Header().Set("Access-Control-Allow-Headers",
+		"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
+
+
+	fmt.Println(req.FormValue("item"))
+
+	fmt.Println(req.FormValue("likes"))
+
+	//converting likes - string to int
+	likes,err := strconv.Atoi(req.FormValue("likes"))
+	if err != nil {
+		log.Fatal(err)
+  } 
+
+
+
+
+
+session, err := mgo.Dial(mongodb_server)
+	if err != nil {
+			panic(err)
+	}
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+	c := session.DB(mongodb_database).C(mongodb_collection)
+
+
+
+  query := bson.M{"item" : req.FormValue("item")}
+  change := bson.M{"$set": bson.M{ "likes" : likes+1 }}
+
+  err = c.Update(query, change)
+  if err != nil {
+		log.Fatal(err)
+  } 
+}
+
+
+
+func main() {
+
+
+http.HandleFunc("/inventoryTeas", inventoryHandlerTeas)
+http.HandleFunc("/cartItemsTeas", cartHandlerTeas)
+http.HandleFunc("/addToCartTeas", starbucksAddToCartHandlerTeas)
+http.HandleFunc("/processOrdersTeas", starbucksProcessOrdersHandlerTeas)
+http.HandleFunc("/likeIncreaseTeas", likeHandlerTeas)
+http.ListenAndServe(":5000", nil)
+}
+
+
